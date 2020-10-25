c515ecd247df05d56772c33258e281649f30aec4
Xiran LYU <xiran.lyu@thoughtworks.com>
2020-10-25 22:16:13 +0800

    third solution for problem 3 added, using @PostConstruct

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,11 +1,18 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Bar {
-
     private Foo foo;
+
+    @PostConstruct
+    public void init() {
+        foo.setBar(this);
+    }
 
     public Bar(Foo foo) {
         this.foo = foo;
