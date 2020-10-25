71ce6e35c72a077cfcee6571b34a7957fa99d90c
Xiran LYU <xiran.lyu@thoughtworks.com>
2020-10-25 22:12:39 +0800

    second solution for problem 3 added, using setter

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,10 +8,11 @@ public class Foo {
 
     private Bar bar;
 
-    public Foo(Bar bar) {
+    @Autowired
+    public void setBar(Bar bar) {
         this.bar = bar;
     }
-
+    
     public void hi() {
         System.out.println("Hi, " + bar.name());
     }
