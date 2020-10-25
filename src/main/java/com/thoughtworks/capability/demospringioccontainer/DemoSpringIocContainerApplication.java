6a291dff767832a26461c7bff6bf8c70e2dc282f
Xiran LYU <xiran.lyu@thoughtworks.com>
2020-10-25 21:39:00 +0800

    first solution for problem 2 added

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/DemoSpringIocContainerApplication.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/DemoSpringIocContainerApplication.java
@@ -2,12 +2,14 @@ package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DemoSpringIocContainerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringIocContainerApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class GreetingController {
-
     private final GreetingService greetingService;
 
     @Autowired
     public GreetingController(GreetingService greetingService) {
+        System.out.println("GreetingController initialized at" + new Date());
         this.greetingService = greetingService;
     }
 
@@ -18,5 +20,4 @@ public class GreetingController {
     public String greet() {
         return greetingService.sayHi();
     }
-
 }
